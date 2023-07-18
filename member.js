daab1fffa11f737ba2fc57f688de155a41d608f7
sri-cloud <60395690+sri-cloud@users.noreply.github.com>
2023-07-18 15:49:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = document.getElementById('member');
+    var skills = document.getElementById('skills');
+    var memberBtn = document.getElementById('memberBtn');
+    var skillsBtn = document.getElementById('skillsBtn');
+    member.style.display = 'block';
+    skills.style.display = 'none';
+    memberBtn.style.backgroundColor = '#fff';
+    skillsBtn.style.backgroundColor = '#f3f3f3';
+}               
+
